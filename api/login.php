c19a523ad3707a3c73ac6f0682a57f38e63238f4
Miika Koppelo <t7komi01@students.oamk.fi>
2018-10-14 17:19:52 +0300

    Kirjautuminen saatiin toimimaan

--- api/login.php
+++ api/login.php
@@ -3,22 +3,22 @@ require "connection.php";
 	$username=$_POST['username'];
   $password=$_POST['password'];
 
-  $sql = $db->prepare("SELECT idTunnus FROM Tunnus WHERE Ktunnus = :Ktunnus AND Salasana = :Salasana");
+  $sql = $db->prepare("SELECT idTunnus FROM Tunnus WHERE Ktunnus = :username AND Salasana = :password");
   $sql->bindParam(':username',$username);
   $sql->bindParam(':password',$password);
   $sql->execute();
 	$result = $sql->fetch(PDO::FETCH_ASSOC);
 
-    $success = $result['idUsers'];
+    $success = $result['idTunnus'];
 
     if($success > 0){
         session_start();
         $_SESSION['username']=$username;
-        $_SESSION['idUsers']=$success;
+        $_SESSION['idTunnus']=$success;
         header("Location: ../ui/index.php");
         exit();
     }
     else {
-      header("Location: ../har10/ui/login.html");
+    header("Location: ../ui/login.html");
     }
 ?>
