8619a63ea3761159c7eefcb990498da0d2fa3c71
CodeFluctus <t7aaju00@students.oamk.fi>
2018-10-15 22:42:22 +0300

    Noutaa kyseisen henkilöon tilit.

--- api/get_tilit.php
+++ api/get_tilit.php
@@ -1,11 +1,17 @@
 <?php
-require "connection.php";
+include "errorDetector.php";
+include "connection.php";
 
 session_start();
 
-$sql = $db->prepare("SELECT Tilinumero, Saldo FROM Pankkitili");
 
-$sql->execute();
+$sql = $db->prepare("SELECT Tilinumero, Saldo FROM Pankkitili
+JOIN Asiakas1_Pankkitili ON Pankkitili.idPankkitili = Asiakas1_Pankkitili.idPankkitili
+JOIN Asiakas1 ON Asiakas1_Pankkitili.idAsiakas = Asiakas1.idAsiakas
+JOIN Tunnus ON Asiakas1.idAsiakas = Tunnus.idAsiakas
+WHERE Ktunnus = ?;");
+
+   $sql->execute(array($_SESSION['username']));
 
     echo"<div class = 'taulukko'>";
     echo "<table border = '1'>
