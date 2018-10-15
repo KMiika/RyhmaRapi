4b845f6db88b0d87518a69223e23124724477b66
CodeFluctus <t7aaju00@students.oamk.fi>
2018-10-15 23:03:11 +0300

    Siistitty index.php koodista vanhat kommentoidut koodit pois.

--- ui/index.php
+++ ui/index.php
@@ -12,10 +12,7 @@
   <?php
   session_start();
     if(isset($_SESSION['username'])){
-      //require "connection.php";
-      //$result = mysqli_query($_SESSION, "SELECT * FROM hae_nimi;");
       echo 'Tervetuloa '.$_SESSION['kokoNimi'].'<br>';
-      //echo 'Tervetuloa '$result'<br>';
       echo '<a href="../api/logout.php"><button>Kirjaudu ulos</button></a>';
     }
     else {
@@ -36,59 +33,14 @@
     <li> <a onclick="loadXMLDoc('../api/get_kortit.php')">Kortit</a> </li>
     <br><h5>Maksut</h5>
     <li> <a onclick="loadXMLDoc('uusimaksu.html')">Uusi maksu</a> </li>
-    <li> <a onclick="loadXMLDoc('omasiirto.php')">Oma siirto tilien välillä</a> </li>
+    <li> <a onclick="loadXMLDoc('../api/omasiirto.php')">Oma siirto tilien välillä</a> </li>
     <li> <a onclick="loadXMLDoc('../api/get_maksu.php')">E-Laskut</a> </li>
-    <li> <a onclick="loadXMLDoc('maksupohjat.php')">Maksupohjat</a> </li>
   </ul>
 
 </div>
-
-<!--<div class="container">
-
-    <p>
-        <button class="btn btn-primary" onclick="GetBooks()">Kaikki kirjat</button>
-
-        Anna kirjan id
-        <input type="text" id="book_id">
-        <button onclick="GetBooks_by_id()">Etsi kirja</button>
-    </p>
-
-    <p>
-        Lisää kirja
-        <br>
-        <form id='BookForm'>
-            <label for="name">Book name</label> <br>
-            <input type="text" name="name" id="name"> <br>
-            <label for="author">Author</label> <br>
-            <input type="text" name="author" id="author"> <br>
-        </form>
-        <button onclick="AddBook()">Lisää kirja</button>
-    </p>
-    <p>
-      Anna poistettavan kirjan id
-      <input type="number" id="delete_id">
-      <button class="btn btn-danger" onclick="DeleteBook()">Poista</button>
-    </p>
-
-    <p>
-        Muokkaa kirja
-        <br>
-        <form id='UpdateForm'>
-          <label for="name">Book id</label> <br>
-          <input type="text" name="idBooks" id="idBooks"> <br>
-            <label for="name">Book name</label> <br>
-            <input type="text" name="name" id="name"> <br>
-            <label for="author">Author</label> <br>
-            <input type="text" name="author" id="author"> <br>
-        </form>
-        <button onclick="UpdateBook()">Tallenna</button>
-    </p>
-
-
-    <div id="results"></div>
-  </div>-->
   <div id="content">
-    Here is content
+    Tervetuloa käyttämään täysin tietoturvallista <br>
+     ja ongelmatonta pankki järjestelmää
   </div>
   </body>
 </html>
