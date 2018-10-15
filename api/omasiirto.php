547b6ce50c400aa63d743c4d2d0f35998f27909d
CodeFluctus <t7aaju00@students.oamk.fi>
2018-10-15 23:02:31 +0300

    Siirretty ../ui -> ../api

---
+++ api/omasiirto.php
@@ -0,0 +1,24 @@
+<h2>Oma siirto</h2>
+
+				<p>
+		        <br>
+						<label>Tililta</label><br>
+						<select name="TilisiForm" id="tilisiform">
+							<option value="Tili1">Tili1</option>
+							<option value="Tili2">Tili2</option>
+							<option value="Tili3">Tili3</option>
+						</select>
+						<br>
+						<label>Tilille</label><br>
+						<select name="TililleForm" id="tililleform">
+							<option value="Tili1">Tili1</option>
+							<option value="Tili2">Tili2</option>
+							<option value="Tili3">Tili3</option>
+						</select>
+						<br><br>
+
+						<label for="Summa">Summa</label> <br>
+            <input type="text" name="Summa" id="Summa"> <br>
+						<br>
+						<input type="submit"value="Siirra summa">
+		    </p>
