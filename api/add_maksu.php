32d972a34d643f9490e835c3df30fb0f39307112
CodeFluctus <t7aaju00@students.oamk.fi>
2018-10-15 17:51:48 +0300

    Maksun suorittaminen toimii nyt

--- api/add_maksu.php
+++ api/add_maksu.php
@@ -1,5 +1,6 @@
 <?php
   include 'connection.php';
+  include "errorDetector.php";
 
   $Tilinro=$_POST['Tilinro'];
   $Nimi=$_POST['Nimi'];
@@ -9,24 +10,17 @@
   $Summa=$_POST['Summa'];
   $Pankkitili=$_POST['Pankkitili'];
 
-  $sql = $db->prepare("INSERT INTO Maksu VALUES (?,?,?,?,?,?,?)");
+  
+      //Toimii ja lisää maksun kyseisen käyttäjän tilille.
+
+  $sql = $db->prepare("INSERT INTO Maksu VALUES (?,?,?,?,?,?,?,?)");
 
       //Suoritetaan valmisteltu proserduuri
 
-      $proseduuri -> execute(array($Tilinro, $Nimi,$Viesti,$Viite,$Eräpäivä,$Summa,$Pankkitili));
+      $sql -> execute(array(NULL, $Tilinro, $Nimi,$Viesti,$Viite,$Eräpäivä,$Summa,$Pankkitili));
 
       $yhteys = null;
       echo "Maksu suoritettu";
-  /*$sql=$db->prepare("INSERT INTO Maksu (Tilinro,Nimi,Viesti,Viite,Eräpäivä,Summa,Pankkitili)VALUES(:a_Tilinro,:a_Nimi,:a_Viesti,:a_Viite,:a_Eräpäivä,:a_Summa,:a_Pankkitili)");
-  $sql->bindParam(':a_Tilinro',$Tilinro);
-  $sql->bindParam(':a_Nimi',$Nimi);
-  $sql->bindParam(':a_Viesti',$Viesti);
-  $sql->bindParam(':a_Viite',$Viite);
-  $sql->bindParam(':a_Eräpäivä',$Eräpäivä);
-  $sql->bindParam(':a_Summa',$Summa);
-  $sql->bindParam(':a_Pankkitili',$Pankkitili);
-  $sql->execute();
-  */
 
   http_response_code(201);
 ?>
