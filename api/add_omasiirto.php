9d9683b8bb4ca29731b9a4e33e37208c31f60028
CodeFluctus <t7aaju00@students.oamk.fi>
2018-10-15 23:04:13 +0300

    Lisätty pohja omalle siirrolle, joka ei vielää täysin toimi tässä vaiheessa.

--- api/add_omasiirto.php
+++ api/add_omasiirto.php
@@ -3,22 +3,7 @@
   include "errorDetector.php";
 
 
-/*  try
-    {
-        $yhteys = new PDO("mysql: host=localhost; dbname=t7aaju00", "t7aaju00", "salasana");
-    }
-    catch(PDOException $e)
-    {
-        die("Virhe: " . $e->getMessage());
-    }
 
-    //Virheenkäsittely: virheet aiheuttavat poikkeuksen
-    $yhteys -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    //Merkistö: Käytetään latin1-merkistöä.(utf8 on myös yleinen)
-    $yhteys ->exec ("SET NAMES latin1"); */
-
-    //valmistellaan proseduuri
     $proseduuri = $yhteys->prepare("CALL UusiSuoritus(?, ?, ?, ?)");
 
     //Suoritetaan valmisteltu proserduuri
